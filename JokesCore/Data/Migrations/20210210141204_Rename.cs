d35c97a4d7802460d0913ce3d99028342e47cabb
Gilles98 <guigilles@gmail.com>
2021-02-11 01:01:50 +0100

    created application and started on main styling + added .gitignore

---
+++ JokesCore/Data/Migrations/20210210141204_Rename.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JokesCore.Data.Migrations
+{
+    public partial class Rename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserTokens",
+                newName: "AspNetUserTokens",
+                newSchema: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "AspNetUsers",
+                newSchema: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserRoles",
+                newName: "AspNetUserRoles",
+                newSchema: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserLogins",
+                newName: "AspNetUserLogins",
+                newSchema: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserClaims",
+                newName: "AspNetUserClaims",
+                newSchema: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoles",
+                newName: "AspNetRoles",
+                newSchema: "JokesCore");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoleClaims",
+                newName: "AspNetRoleClaims",
+                newSchema: "JokesCore");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "AspNetUserTokens",
+                schema: "JokesCore",
+                newName: "AspNetUserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                schema: "JokesCore",
+                newName: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserRoles",
+                schema: "JokesCore",
+                newName: "AspNetUserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserLogins",
+                schema: "JokesCore",
+                newName: "AspNetUserLogins");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserClaims",
+                schema: "JokesCore",
+                newName: "AspNetUserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoles",
+                schema: "JokesCore",
+                newName: "AspNetRoles");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoleClaims",
+                schema: "JokesCore",
+                newName: "AspNetRoleClaims");
+        }
+    }
+}
